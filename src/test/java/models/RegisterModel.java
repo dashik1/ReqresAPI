bc9855150db6e283a67a158d9f8e3b6c036688c5
Dasha <darya.pashyk@gmail.com>
2022-10-09 10:09:15 +0400

    HW_18

---
+++ src/test/java/models/RegisterModel.java
@@ -0,0 +1,13 @@
+package models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+
+public class RegisterModel {
+
+    private String email;
+    private String password;
+}
--- src/test/java/restassured/RestAssuredTest.java
+++ src/test/java/restassured/RestAssuredTest.java
@@ -3,12 +3,14 @@ package restassured;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import models.LoginModel;
+import models.RegisterModel;
+import models.UpdateUserModel;
 import org.testng.annotations.Test;
 
 import java.io.File;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.*;
 
 public class RestAssuredTest {
 
@@ -76,6 +78,123 @@ public class RestAssuredTest {
                 .put("https://reqres.in/api/users/2")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void updateUserNameTest() {
+        UpdateUserModel updateUserName = UpdateUserModel
+                .builder()
+                .name("Dasha")
+                .build();
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(updateUserName)
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void deleteUserTest() {
+        RestAssured
+                .given()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    public void registerSuccessTest() {
+        RegisterModel registerModel = RegisterModel
+                .builder()
+                .email("eve.holt@reqres.in")
+                .password("pistol")
+                .build();
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(registerModel)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void registerUnSuccessTest() {
+        RegisterModel registerModel = RegisterModel
+                .builder()
+                .email("sydney@fife")
+                .build();
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(registerModel)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", equalTo("Missing password"));
+    }
+
+    @Test
+    public void loginSuccessTest() {
+        LoginModel loginModel = LoginModel
+                .builder()
+                .email("eve.holt@reqres.in")
+                .password("cityslicka")
+                .build();
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(loginModel)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body(containsString("token"));
+    }
+
+    @Test
+    public void loginUnSuccessTest() {
+        LoginModel loginModel = LoginModel
+                .builder()
+                .email("eve.holt@reqres.in")
+                .build();
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(loginModel)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", equalTo("Missing password"));
+    }
+
+    @Test
+    public void getDelayedResponseTest() {
+        JsonPath expectedJson = new JsonPath(new File("src/test/resources/users.json"));
+        RestAssured
+                .given()
+                .when()
+                .get("https://reqres.in/api/users?delay=3")
+                .then()
+                .statusCode(200)
+                .body("", equalTo(expectedJson.getMap("")));
     }
 
 
